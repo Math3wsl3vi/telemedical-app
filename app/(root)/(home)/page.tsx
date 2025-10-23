c2ef853fc97abc17a45173305b2dc4617a8e7286
math3wsl3vi <levimathews9841@gmail.com>
2025-10-23 12:19:28 +0300

    page edit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen w-full text-black bg-white font-poppins overflow-hidden'>
+    <div className='h-screen w-full text-black bg-white font-poppins'>
         <Hero/>
     </div>
   )
